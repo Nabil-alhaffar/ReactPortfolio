254d443aa2b1186ed13b0bd68635341f0507c824
System Administrator <root@Nabils-MBP.lan>
2024-04-02 00:30:19 -0700

    Added Sounds

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import SoundProvider from './components/SoundProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-       <App />
+        <SoundProvider/>
+         <App />
     </BrowserRouter>
     
   </React.StrictMode>
